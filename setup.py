c1477c1d8d0d4889a5f03826327fcf1bbdde18ef
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2016-09-20 20:30:25 +0900

    modified app name

--- setup.py
+++ setup.py
@@ -19,9 +19,11 @@ OPTIONS = {
         'app/resources'
     ]
 }
+NAME='Jupyter Notebook Launcher'
 
 setup(
     app=APP,
+    name=NAME,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
